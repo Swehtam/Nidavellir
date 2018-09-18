ec42d91289989bd2b145954a889fef53b1d5d545
ArmandoTGT <armando_tgt@hotmail.com>
2018-09-18 00:12:04 -0300

    Ranger Chase

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -6,21 +6,55 @@ public class EnemyController : MonoBehaviour {
 
     private Rigidbody myRB;
     public float moveSpeed;
+	public float range;
+	private bool seeker = false;
+	private Animator anim;
+	private Vector3 moveInput;
+	private bool enemyMoving;
+	private Vector2 lastMove;
 
-    private PlayerControl thePlayer;
+
+	private PlayerControl thePlayer;
 	// Use this for initialization
-	void Start () {
-        myRB = GetComponent<Rigidbody>();
-        thePlayer = FindObjectOfType<PlayerControl>();
+
+	void Start () {  
+		myRB = GetComponent<Rigidbody>();
+		thePlayer = FindObjectOfType<PlayerControl>();
+		anim = GetComponent<Animator>();
+
 	}
 
     private void FixedUpdate()
     {
-        myRB.velocity = transform.forward * moveSpeed;
+		if (Vector3.Distance(thePlayer.transform.position, transform.position) <= range || seeker) {
+			seeker = true;
+			
+			myRB.velocity = transform.forward * moveSpeed;
+		}
     }
 
     // Update is called once per frame
     void Update () {
-        transform.LookAt(thePlayer.transform.position);
+		if (seeker) {
+
+			enemyMoving = false;
+
+			moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+			moveInput = moveInput * moveSpeed;
+
+
+			if (moveInput.x != 0 || moveInput.z != 0)
+			{
+				enemyMoving = true;
+				lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+			}
+			anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+			anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+			anim.SetBool("EnemyMoving", enemyMoving);
+			anim.SetFloat("LastMoveX", lastMove.x);
+			anim.SetFloat("LastMoveY", lastMove.y);
+
+			transform.LookAt (new Vector3 (thePlayer.transform.position.x, transform.position.y, thePlayer.transform.position.z));
+		}
 	}
 }
