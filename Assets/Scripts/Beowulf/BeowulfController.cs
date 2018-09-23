720df38b609e47310a2f13bb5835097328fbc491
ArmandoTGT <armando_tgt@hotmail.com>
2018-09-23 19:26:10 -0300

    Beowulf andando até ponto especifico

--- Assets/Scripts/Beowulf/BeowulfController.cs
+++ Assets/Scripts/Beowulf/BeowulfController.cs
@@ -2,58 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour
-{
+public class BeowulfController : MonoBehaviour {
 
 
-
+	
 	public float moveSpeed;
 	public float maxRange;
-	public float minRange;
+    public float minRange;
 	private bool seeker = false;
 	private Animator anim;
-	private bool enemyMoving;
+	private bool enemyMoving = true;
 	private Vector2 direction;
-	private float step;
+    private float step;
 	private Rigidbody2D myRB;
 	private float xDir;
 	private float yDir;
 
-	private PlayerControl thePlayer;
+	public GameObject point;
 	// Use this for initialization
 
-	void Start()
-	{
-		myRB = GetComponent<Rigidbody2D>();
-		thePlayer = FindObjectOfType<PlayerControl>();
+	void Start () {
+		myRB = GetComponent<Rigidbody2D>();		
 		anim = GetComponent<Animator>();
 	}
 
-	private void FixedUpdate()
-	{
-		if (enemyMoving)
-		{
-			step = moveSpeed * Time.deltaTime;
+    private void FixedUpdate()
+    {
+        if (enemyMoving)
+        {
+            step = moveSpeed * Time.deltaTime;
 			//myRB.velocity = direction;
-			transform.position = Vector2.MoveTowards(transform.position, thePlayer.transform.position, step);
-		}
+			transform.position = Vector2.MoveTowards(transform.position, point.transform.position, step);
+        }
+        
+    }
 
-	}
+    // Update is called once per frame
+    void Update () {       
 
-	// Update is called once per frame
-	void Update()
-	{
-		enemyMoving = false;
-
-		//se o personagem entrar na area de ameaça do inimigo, o inimigo irá segui-lo
-		if (Vector2.Distance(thePlayer.transform.position, transform.position) <= maxRange || seeker)
-		{
-			seeker = true;
-			enemyMoving = true;
+        //se o personagem entrar na area de ameaça do inimigo, o inimigo irá segui-lo
+      
 
 			//vetor para saber qual a posição do inimigo para o player
-			xDir = thePlayer.transform.position.x - transform.position.x;
-			yDir = thePlayer.transform.position.y - transform.position.y;
+			xDir = point.transform.position.x - transform.position.x;
+			yDir = point.transform.position.y - transform.position.y;
 			/*
 			//saber se o inimigo olha pra esquerda ou pra direita,
 			//se for perto de 0, então desconsidera o x e só considera a posição z (do eixo do mundo)
@@ -69,6 +61,7 @@ public class EnemyController : MonoBehaviour
             {
                 direction.x = -1.0f;
             }
+
             //saber se o inimigo olha pra cima ou pra baixo,
             //se for perto de 0, então desconsidera o z (do eixo do mundo) e só considera a posição x 
             if (direction.y > -1.0f && direction.y < 1.0f)
@@ -84,22 +77,21 @@ public class EnemyController : MonoBehaviour
                 direction.y = -1.0f;
             }
 			*/
-			//caso o inimigo fique muito proximo, faço o parar na frente do player e olhar na direção dele
-			//isso aqui foi feito para o inimigo n ficar empurrando o player para fora do mapa
-			if (Vector2.Distance(thePlayer.transform.position, transform.position) <= minRange)
-			{
-				enemyMoving = false;
+            //caso o inimigo fique muito proximo, faço o parar na frente do player e olhar na direção dele
+            //isso aqui foi feito para o inimigo n ficar empurrando o player para fora do mapa
+            if (Vector2.Distance(point.transform.position, transform.position) <= minRange){
+                enemyMoving = false;
 
-				anim.SetFloat("LastMoveX", xDir);
-				anim.SetFloat("LastMoveY", yDir);
-			}
-			else
-			{
+                anim.SetFloat("LastMoveX", xDir);
+                anim.SetFloat("LastMoveY", yDir);
+            }
+            else
+            {				
 				anim.SetFloat("MoveX", xDir);
-				anim.SetFloat("MoveY", yDir);
-			}
-
+                anim.SetFloat("MoveY", yDir);
+            }	
+            
 			anim.SetBool("EnemyMoving", enemyMoving);
-		}
+        }
 	}
-}
+
