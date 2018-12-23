9389c59ba043eee02b7e93bf4cd7e4324ec2d613
Swehtam <mathewsalves1@outlook.com]>
2018-12-23 13:58:08 -0200

    implementação das opções no menu inicial

--- Assets/Scripts/Menu/SettingsMenu.cs
+++ Assets/Scripts/Menu/SettingsMenu.cs
@@ -9,6 +9,7 @@ public class SettingsMenu : MonoBehaviour
     public AudioMixer audioMixer;
 
     public Dropdown resolutionDropdown;
+    public Dropdown graphicsDropdown;
 
     Resolution[] resolutions;
 
@@ -19,12 +20,14 @@ public class SettingsMenu : MonoBehaviour
 
         List<string> options = new List<string>();
 
+        // Loop para criar as strings do dropdown da resolução
         int currentResolutionIndex = 0;
         for(int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
 
+            // Condição para descobrir o tamanho de tela atual do player
             if (resolutions[i].width == Screen.currentResolution.width && 
                 resolutions[i].height == Screen.currentResolution.height)
             {
@@ -32,41 +35,50 @@ public class SettingsMenu : MonoBehaviour
             }
         }
 
+        // Adicionar as opções de resolução e setar o tamanho atual da tela do player
         resolutionDropdown.AddOptions(options);
         resolutionDropdown.value = currentResolutionIndex;
         resolutionDropdown.RefreshShownValue();
+
+        // Setar o grafico inicial do jogo (ULTRA)
+        graphicsDropdown.value = 5;
+        graphicsDropdown.RefreshShownValue();
     }
 
+    // Metodo para mudar a resolução quando selecionar um valor no dropdown
     public void SetResolution(int resolutionIndex)
     {
         Resolution resolution = resolutions[resolutionIndex];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
+    // Metodo para mudar o volume geral do jogo quando mudar o slidebar
     public void SetMasterVolume(float volume)
     {
         audioMixer.SetFloat("masterVolume", volume);
     }
 
+    // Metodo para mudar o volume da música do jogo quando mudar o slidebar
     public void SetMusicVolume(float volume)
     {
         audioMixer.SetFloat("musicVolume", volume);
     }
 
+    // Metodo para mudar o volume dos efeitos sonoros do jogo quando mudar o slidebar
     public void SetSoundEffectsVolume(float volume)
     {
         audioMixer.SetFloat("soundEffectsVolume", volume);
     }
 
+    // Metodo para mudar a qualidade quando selecionar um valor no dropdown
     public void SetQuality(int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
     }
 
+    // Metodo para colocar a tela fullscreen ou não
     public void SetFullscreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
     }
-
-
 }
